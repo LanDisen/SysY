272da8578d9dc4d883078d5d41450a8b9db316b5
Lann <3530306071@qq.com>
2022-10-15 23:15:12 +0800

    添加Error类，用于报错显示

--- src/Error.java
+++ src/Error.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Error {
+public class Error {
+    Error(String message) {
+        report(message);
+        System.exit(-1);
+    }
+
+    void report(String message) {
+        System.out.println("Error: " + message);
+    }
 }
