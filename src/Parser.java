99611e997f4dc0442eedd0766527836e56d77f7e
Lann <3530306071@qq.com>
2022-10-18 19:42:12 +0800

    删除原文法的注释代码

--- src/Parser.java
+++ src/Parser.java
@@ -3,14 +3,6 @@ import java.util.Vector;
 /**
  * 语法分析器
  * 算术表达式文法：
- * expression  ->  term expression'
- * expression' ->  ("+"|"-") term expression' | ε
- * term        ->  factor term'
- * term'       ->  ("*"|"/") factor term' | ε
- * factor      ->  "(" expression ")" | NUMBER
- * 以上文法构造语法树进行语义分析比较困难
- *
- * 试图构造新文法便于扩展，理想：NUMBER op NUMBER
  * expression  ->  term ( ("+"|"-") term )*
  * term        ->  factor ( ("*"|"/") factor )*
  * factor      ->  "(" expression ")" | NUMBER
@@ -20,78 +12,6 @@ public class Parser {
         this.tokens = tokens;
         parse();
     }
-
-    /*
-    void parse() {
-        while (!isParseOver()) {
-            //System.out.println(curr);
-            ExpressionStmt expressionStmt = new ExpressionStmt(expression());
-            if (!expressionStmt.hasError) {
-                statements.add(expressionStmt);
-                System.out.println(expressionStmt);
-            }
-            if (!match(TokenType.SEMICOLON)) {
-                new Error(peek(), "expect ';' here");
-            }
-        }
-    }
-
-    Expr expression() {
-        Expr expr = new NullExpr();
-        Expr termExpr = term();
-        Expr expression1Expr = expression1();
-        expr = new BinaryExpr(termExpr, expression1Expr);
-        return expr;
-    }
-
-    Expr expression1() {
-        Expr expr = new NullExpr();
-        if (match(TokenType.PLUS) || match(TokenType.MINUS)) {
-            Token opt = getPrev();
-            Expr termExpr = term();
-            Expr expression1Expr = expression1();
-            expr = new OptBinaryExpr(opt, termExpr, expression1Expr);
-        }
-        return expr;
-    }
-
-    Expr term() {
-        Expr expr = new NullExpr();
-        Expr factorExpr = factor();
-        Expr term1Expr = term1();
-        expr = new BinaryExpr(factorExpr, term1Expr);
-        return expr;
-    }
-
-    Expr term1() {
-        Expr expr = new NullExpr();
-        //匹配"*"|"/"
-        if (match(TokenType.STAR) || match(TokenType.SLASH)) {
-            Token opt = getPrev();
-            Expr termExpr = term();
-            Expr expression1Expr = expression1();
-            expr = new OptBinaryExpr(opt, termExpr, expression1Expr);
-        }
-        return expr;
-    }
-
-    //factor  ->  "(" expression ")" | NUMBER
-    Expr factor() {
-        Expr expr = new NullExpr();
-        if (match(TokenType.LEFT_BRACKET)) {
-            Expr expressionExpr = expression();
-            if (match(TokenType.RIGHT_BRACKET)) {
-                expr = new ExpressionExpr(expressionExpr, true);
-            }
-        } else if (match(TokenType.NUM)) {
-            expr = new NumberExpr(getPrev().word);
-        } else {
-            expr.hasError = true;
-            new Error(peek(), "this is not a valid arithmetic expression.");
-        }
-        return expr;
-    }
-    */
 
     void parse() {
         while (!isParseOver()) {
