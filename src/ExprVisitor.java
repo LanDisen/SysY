ca5ef71ae535ec69ae6308625669d6ae7f4cdcda
Lann <3530306071@qq.com>
2022-10-18 15:51:51 +0800

    Visitor接口单独作为文件

--- src/ExprVisitor.java
+++ src/ExprVisitor.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public interface ExprVisitor {
+interface ExprVisitor {
+    public Object visitOptBinaryExpr(OptBinaryExpr expr);
+    public Object visitBinaryExpr(BinaryExpr expr);
+    public Object visitExpressionExpr(ExpressionExpr expr);
+    public Object visitNumberExpr(NumberExpr expr);
+    public Object visitNullExpr(NullExpr expr);
+
 }
--- src/StmtVisitor.java
+++ src/StmtVisitor.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public interface StmtVisitor {
+interface StmtVisitor {
+    Object visitExpressionStmt(ExpressionStmt stmt);
+    Object visitPrintStmt(PrintStmt stmt);
 }
